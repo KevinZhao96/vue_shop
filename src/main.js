6bd4651e4169f3c9eac35bf4f444c9778b6c8737
wenkai zhao <wenkai.zhao@meditrusthealth.com>
2021-09-09 17:30:32 +0800

    login page completed

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './plugins/element.js'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./plugins/element.js";
+import "./assets/css/global.css";
+import "./assets/fonts/iconfont.css";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.prototype.$http = axios;
+axios.defaults.baseURL = "https://lianghj.top:8888/api/private/v1/";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,40 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Login from "../components/Login.vue";
+import Home from "../components/Home.vue";
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    redirect: "/login"
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: "/login",
+    component: Login
+  },
+  {
+    path: "/home",
+    component: Home
   }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  // to and from are both route objects. must call `next`.
+  // 如果地址为login则放行
+  if (to.path === "/login") {
+    return next();
+  }
+  // 如果没有token则返回login页面
+  const tokenStr = window.sessionStorage.getItem("token");
+  if (!tokenStr) {
+    return next("/login");
+  }
+  //如果有token则放行
+  next();
+});
+export default router;
